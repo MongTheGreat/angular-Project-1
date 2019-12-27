95fceb4efb987328179f2f8b5d95432d113d7acc
MongTheGreat <mohlankam@gmail.com>
2019-12-27 15:10:16 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -9,8 +10,9 @@ import{ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star/star.component';
 import { ProductServiceService } from './product-list/product-service.service';
 
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule ,HttpClientModule],
   declarations: [ AppComponent, HelloComponent, ProductListComponent,ConvertToSpacesPipe, StarComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ProductServiceService]
--- src/app/product-list/product-service.service.ts
+++ src/app/product-list/product-service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {IProduct} from './product';
+import{catchError,tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn:'root'
@@ -11,8 +12,11 @@ export class ProductServiceService {
   private prodcutUrl = 'api/products/products.json';
   constructor(private http: HttpClient) { }
   getProdcust(): Observable<IProduct[]>{
-    return this.http.get<IProduct[]>(this.prodcutUrl);
+    return this.http.get<IProduct[]>(this.prodcutUrl.pipe(
+      tap(data => console.log('All: '+ JSON.stringify(data))),
+      catchError(this.handleError)
+    );
   }
-  
+  private handleError(err:HttpErrorRespone)
 
 }
