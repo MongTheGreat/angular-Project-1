c13b18f5bee4a6e9cd7cc1589c94c04101fa73a9
MongTheGreat <mohlankam@gmail.com>
2019-12-27 15:28:27 +0200

    1 file modified

--- src/app/product-list/product-service.service.ts
+++ src/app/product-list/product-service.service.ts
@@ -1,25 +1,33 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {IProduct} from './product';
-import{catchError,tap} from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap, map } from 'rxjs/operators';
+
+import { IProduct } from './product';
 
 @Injectable({
-  providedIn:'root'
+  providedIn: 'root'
 })
-export class ProductServiceService {
+export class ProductService {
+  // If using Stackblitz, replace the url with this line
+  // because Stackblitz can't find the api folder.
+  // private productUrl = 'assets/products/products.json';
+  private productUrl = 'api/products/products.json';
 
-  private prodcutUrl = 'api/products/products.json';
   constructor(private http: HttpClient) { }
-  getProdcust(): Observable<IProduct[]>{
-    return this.http.get<IProduct[]>(this.prodcutUrl).pipe(
-                tap(data=> console.log('All: ' + JSON.stringify(data)))
-                catchError(this.handleError)
 
-
-    );
-   
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this.productUrl)
+      .pipe(
+        tap(data => console.log('All: ' + JSON.stringify(data))),
+        catchError(this.handleError)
+      );
   }
-  private handleError(err: HttpErrorResponse){
+
+  
+
+  private handleError(err: HttpErrorResponse) {
+    
+  }
 
 }
