2815c495b4c10e28b2504ea727b1088a83838c2c
MongTheGreat <mohlankam@gmail.com>
2020-01-16 21:38:24 +0200

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -9,12 +10,18 @@ import { ProductListComponent } from './product-list/product-list.component';
 import{ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star/star.component';
 import { ProductService } from './product-list/product.service';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ,HttpClientModule],
-  declarations: [ AppComponent, HelloComponent, ProductListComponent,ConvertToSpacesPipe, StarComponent ],
+  imports:      [ BrowserModule, FormsModule ,HttpClientModule,
+  RouterModule.forRoot([
+      { path: 'product-list',pathMatch: 'full', component: ProductListComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]),],
+  declarations: [ AppComponent, HelloComponent, ProductListComponent,ConvertToSpacesPipe, StarComponent, WelcomeComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ProductService],
  
